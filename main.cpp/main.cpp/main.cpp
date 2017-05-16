1990a2fe360f8d7a198882378687928b5b6fd37b
kafkaphoenix <jaguilerapuerta@gmail.com>
2017-05-16 17:32:27 +0200

    I finally linked the libs

---
+++ main.cpp/main.cpp/main.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+
+#include <SDL.h>
+
+#include <GL\glew.h>
+
+#include <SDL_opengl.h>
+
+const GLint WIDTH = 800, HEIGHT = 600;
+
+int main(int argc, char *argv[])
+{
+	SDL_Init(SDL_INIT_EVERYTHING);
+
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+	SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
+
+	SDL_Window *window = SDL_CreateWindow("SDL OpenGL", 0, 0, WIDTH, HEIGHT, SDL_WINDOW_OPENGL);
+
+	SDL_GLContext context = SDL_GL_CreateContext(window);
+
+	glewExperimental = GL_TRUE;
+
+	if (GLEW_OK != glewInit())
+	{
+		std::cout << "Failed to initialize GLEW" << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	glViewport(0, 0, WIDTH, HEIGHT);
+
+	SDL_Event windowEvent;
+
+	while (true) {
+		if (SDL_PollEvent(&windowEvent))
+		{
+			if (SDL_QUIT == windowEvent.type)
+			{
+				break;
+			}
+		}
+
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		//Draw OpenGL
+		SDL_GL_SwapWindow(window);
+	}
+
+	SDL_GL_DeleteContext(context);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+
+	return EXIT_SUCCESS;
+}
