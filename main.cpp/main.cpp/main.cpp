f9e179f8592df448a4cf39a5e60077e4a524b06d
kafkaphoenix <jaguilerapuerta@gmail.com>
2017-05-16 18:08:36 +0200

    GLM! net! ttf! image!

--- main.cpp/main.cpp/main.cpp
+++ main.cpp/main.cpp/main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
-
 #include <SDL.h>
-
 #include <GL\glew.h>
-
 #include <SDL_opengl.h>
+#include <SDL_net.h>
+#include <SDL_image.h>
+#include <SDL_ttf.h>
+
 
 const GLint WIDTH = 800, HEIGHT = 600;
 
