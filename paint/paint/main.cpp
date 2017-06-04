454be238621a07159a6aabf3574f73aae1167a64
kafkaphoenix <jaguilerapuerta@gmail.com>
2017-06-04 20:20:53 +0200

    done! push supr to delete drawing

---
+++ paint/paint/main.cpp
@@ -0,0 +1,87 @@
+#include <SDL.h>
+#include <iostream>
+
+int main(int argc, char ** argv) {
+	bool leftMouseButtonDown = false;
+	bool quit = false;
+	SDL_Event event;
+
+	SDL_Init(SDL_INIT_VIDEO);
+
+	SDL_Window *window = SDL_CreateWindow("SDL 2 Pixel Drawing", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, 0);
+
+	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
+	SDL_Texture *texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ABGR8888, SDL_TEXTUREACCESS_STATIC, 640, 480);
+	Uint32 *pixels = new Uint32[640 * 480];
+	memset(pixels, 255, 640 * 480 * sizeof(Uint32));
+
+	while (!quit) {
+		SDL_UpdateTexture(texture, NULL, pixels, 640 * sizeof(Uint32));
+
+		SDL_WaitEvent(&event);
+
+		switch (event.type)
+		{
+		case SDL_KEYDOWN:
+			switch (event.key.keysym.sym) 
+			{
+				case SDLK_ESCAPE:
+					quit = true;
+					break;
+				case SDLK_DELETE:
+					memset(pixels, 255, 640 * 480 * sizeof(Uint32));
+					break;
+			}
+			break;
+		case SDL_QUIT:
+			quit = true;
+			break;
+		case SDL_MOUSEBUTTONUP:
+			if (event.button.button == SDL_BUTTON_LEFT)
+				leftMouseButtonDown = false;
+			break;
+		case SDL_MOUSEBUTTONDOWN:
+			if (event.button.button == SDL_BUTTON_LEFT) 
+				leftMouseButtonDown = true;
+		case SDL_MOUSEMOTION:
+			if (leftMouseButtonDown)
+			{
+				int mouseX = event.motion.x;
+				int mouseY = event.motion.y;
+				//center
+				pixels[mouseY * 640 + mouseX] = 0;
+				//up
+				pixels[(mouseY-1) * 640 + mouseX] = 0;
+				//down
+				pixels[(mouseY+1) * 640 + mouseX] = 0;
+				//left
+				pixels[mouseY * 640 + (mouseX-1)] = 0;
+				//right
+				pixels[mouseY * 640 + (mouseX+1)] = 0;
+				//corner 1
+				pixels[(mouseY - 1) * 640 + (mouseX+1)] = 0;
+				//corner 2
+				pixels[(mouseY - 1) * 640 + (mouseX - 1)] = 0;
+				//corner 3
+				pixels[(mouseY + 1) * 640 + (mouseX - 1)] = 0;
+				//corner 4
+				pixels[(mouseY + 1) * 640 + (mouseX + 1)] = 0;
+			}
+			break;
+		}
+
+		SDL_RenderClear(renderer);
+		SDL_RenderCopy(renderer, texture, NULL, NULL);
+		SDL_RenderPresent(renderer);
+		
+	}
+
+	delete[] pixels;
+	SDL_DestroyTexture(texture);
+	SDL_DestroyRenderer(renderer);
+
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+
+	return 0;
+}
