6da3ca4e55ba26d4c5bf005586e5e1d18c5e30eb
kafkaphoenix <jaguilerapuerta@gmail.com>
2017-06-04 14:43:16 +0200

    Loading images using SDL_image.h

--- main.cpp/main.cpp/main.cpp
+++ main.cpp/main.cpp/main.cpp
@@ -1,74 +1,54 @@
-#include <iostream>
-#include <SDL.h>
-#include <GL\glew.h>
-#include <SDL_opengl.h>
-#include <SDL_net.h>
+#include <SDL.h>    
 #include <SDL_image.h>
-#include <SDL_ttf.h>
 
+#include <iostream>
 
-const GLint WIDTH = 800, HEIGHT = 600;
-
-int main(int argc, char *argv[])
+int main(int argc, char ** argv)
 {
-	SDL_Init(SDL_INIT_EVERYTHING);
+	bool quit = false;
+	SDL_Event event;
 
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
-	SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
+	SDL_Init(SDL_INIT_VIDEO);
+	SDL_Init(IMG_INIT_JPG);
+	//SDL_Init(IMG_INIT_PNG);
 
-	SDL_Window *window = SDL_CreateWindow("SDL OpenGL", 0, 0, WIDTH, HEIGHT, SDL_WINDOW_OPENGL);
+	SDL_Window * window = SDL_CreateWindow("SDL2 Displaying Image",
+		SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, 0);
 
-	SDL_GLContext context = SDL_GL_CreateContext(window);
-
-	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
-
-	SDL_Surface *image = SDL_LoadBMP("image.bmp");
-
-	if (image == NULL) {
-		std::cout << "SDL could not  load image" << std::endl << SDL_GetError() << std::endl;
+	SDL_Renderer * renderer = SDL_CreateRenderer(window, -1, 0);
+	SDL_Surface * image = IMG_Load("image.jpg");
+	//SDL_Surface * image = SDL_LoadBMP("image.bmp");
+	//SDL_Surface * image = IMG_Load("container.jpg");
+	//SDL_Surface * image = IMG_Load("awesomeface.png");
+	if (image == nullptr) {
+		std::cout << "Failed to load image" << std::endl << SDL_GetError() << std::endl;
 	}
+	SDL_Texture * texture = SDL_CreateTextureFromSurface(renderer, image);
 
-	SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, image);
-
-	glewExperimental = GL_TRUE;
-
-	if (GLEW_OK != glewInit())
+	while (!quit)
 	{
-		std::cout << "Failed to initialize GLEW" << std::endl;
-		return EXIT_FAILURE;
-	}
+		SDL_WaitEvent(&event);
 
-	glViewport(0, 0, WIDTH, HEIGHT);
-
-	SDL_Event windowEvent;
-
-	while (true) {
-		if (SDL_PollEvent(&windowEvent))
+		switch (event.type)
 		{
-			if (SDL_QUIT == windowEvent.type)
-			{
-				break;
-			}
+		case SDL_QUIT:
+			quit = true;
+			break;
 		}
 
-		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
-
+		//SDL_Rect dstrect = { 5, 5, 320, 240 };
+		//SDL_RenderCopy(renderer, texture, NULL, &dstrect);
 		SDL_RenderCopy(renderer, texture, NULL, NULL);
 		SDL_RenderPresent(renderer);
-
-		//Draw OpenGL
-		//SDL_GL_SwapWindow(window);
 	}
 
 	SDL_DestroyTexture(texture);
 	SDL_FreeSurface(image);
 	SDL_DestroyRenderer(renderer);
-	SDL_GL_DeleteContext(context);
 	SDL_DestroyWindow(window);
+
+	IMG_Quit();
 	SDL_Quit();
 
-	return EXIT_SUCCESS;
+	return 0;
 }
